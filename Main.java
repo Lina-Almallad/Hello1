97caf08961860f6abb71f21c38e143ecbba656bb
Lina <linaalmallad>
2022-02-03 15:17:34 +0200

    First program

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Welcome to Java");
     System.out.println(3*5);
-    System.out.println(7*9);
+    System.out.println(7*2);
     }
 }
