f6ea8ad415b6395992b694b4f71abcbaab8f8313
Lina <linaalmallad>
2022-02-03 15:02:47 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Welcome to Java");
+    System.out.println(3*5);
+    System.out.println(7*9);
+    }
+}
